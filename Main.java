f79561cda75b503c93f12e4a0ff97446a2336a9a
gabrielgarc <122548028+gabrielgarcia-01@users.noreply.github.com>
2023-02-26 21:53:27 +0000

    Prints out length of the word entered

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
     System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
+    String word = input.nextLine();
+    System.out.println(word.length());
   }
 }
